d5cd5e567cd6b23d2fc46958e0dd82d197590379
yashc1998 <yashc1998@users.noreply.github.com>
2017-12-28 13:02:00 +0530

    Test Project

--- app/src/main/java/com/example/yashc/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/yashc/loginscreen/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.example.yashc.loginscreen;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputEditText username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
