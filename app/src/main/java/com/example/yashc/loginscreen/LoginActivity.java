915f8b0bb5895ba88fddd9a0f8a72813aa46b7dd
yashc1998 <yashc1998@users.noreply.github.com>
2017-12-28 13:17:53 +0530

    Test Project

--- app/src/main/java/com/example/yashc/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/yashc/loginscreen/LoginActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputEditText username;
+    TextInputEditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
